144494f7a0bd240463ea8d5157bbdc2219ca1ab3
Philipp <pengelmann92@googlemail.com>
2019-08-09 22:21:31 +0200

    Add missing version insert script

--- src/webapp/db/scripts/1.1.sql
+++ src/webapp/db/scripts/1.1.sql
@@ -1,1 +1,4 @@
 CREATE TABLE dbo.temperature (id serial PRIMARY KEY, temperature numeric(6, 2), create_date TIMESTAMP NOT NULL DEFAULT CURRENT_DATE)
+
+INSERT INTO dbo.versions (name)
+VALUES ('1.1')
